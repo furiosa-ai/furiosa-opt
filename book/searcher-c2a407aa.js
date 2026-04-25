5b62c2102f0d90f06f6956086ea98fa10db2e3da
scho-furiosa <scho-furiosa@users.noreply.github.com>
2026-04-25 05:10:51 +0000

    deploy: a837582f3ed8f9ca81033930f49a50668f45b216

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-92587d88.js',
+                path_to_root + 'searchindex-56bb6057.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
